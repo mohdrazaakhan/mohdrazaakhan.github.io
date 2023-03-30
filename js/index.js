1825f4c7adebe9c5af3e2c2e7455102ed70fbd31
mohdrazaakhan <razaakhan72@gmail.com>
2023-03-30 13:37:13 +0530

    resume linked fixed

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("#resumeButton");
+button.addEventListener("click" , handleClickFunction );
+
+function handleClickFunction (){
+    // alert("I am clicked");
+    window.open("https://drive.google.com/file/d/1AuR9cIF0sWu01eeBTt4AmQVVXHPCI-c7/view?usp=share_link" , "_blank" );
+
+    //https://drive.google.com/file/d/1AuR9cIF0sWu01eeBTt4AmQVVXHPCI-c7/view?usp=share_link
+}
