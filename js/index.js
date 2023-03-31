a2f7088608c6b0e6b28c528643d1fc0a4e7930aa
mohdrazaakhan <razaakhan72@gmail.com>
2023-03-31 16:58:22 +0530

    All done

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ button.addEventListener("click" , handleClickFunction );
 
 function handleClickFunction (){
     // alert("I am clicked");
-    window.open("https://drive.google.com/file/d/1AuR9cIF0sWu01eeBTt4AmQVVXHPCI-c7/view?usp=share_link" , "_blank" );
+    window.open("https://drive.google.com/file/d/1bkv-7NnWek68XhWc2-CZM47ZD-1jMCzP/view?usp=share_link" , "_blank" );
 
     //https://drive.google.com/file/d/1AuR9cIF0sWu01eeBTt4AmQVVXHPCI-c7/view?usp=share_link
 }
